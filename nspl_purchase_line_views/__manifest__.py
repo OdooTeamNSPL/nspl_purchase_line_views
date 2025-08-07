bb16536c26324ae089a40d4cf4695e42fae63f4f
Team Odoo NSPL <odoo_team@namahsoftech.com>
2025-08-07 12:14:57 +0530

    Add files via upload

---
+++ nspl_purchase_line_views/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'Purchase Order Line View',
+    'version': '18.0',
+    'summary': """
+    Access purchase order lines and RFQs through various intuitive views.
+    """,
+    'description': """
+✔ View Purchase Orders and RFQ Order Lines in a centralized interface.
+✔ Includes Tree, Kanban, Calendar, Pivot, and Graph views.
+✔ Effortlessly navigate between different perspectives for better visualization and analysis.
+
+""",
+    'category': 'Purchases',
+    'sequence': 1,
+    'author': 'Namah Softech Private Limited',
+    'website': 'https://www.namahsoftech.com/',
+    'license': 'LGPL-3',
+    'price': 24.99,
+    'currency': 'USD',
+    'support': 'support@namahsoftech.com',
+    'contributors': ["Rutik Patil"],
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_order_line_views.xml',
+        'views/rfq_line_views.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
